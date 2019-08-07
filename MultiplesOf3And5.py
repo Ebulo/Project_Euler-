dc164334b9f7bbd941a196c51b336aa1a302a08a
Ebulo <48652521+Ebulo@users.noreply.github.com>
2019-08-07 19:49:21 +0530

    Project_Eular
    This is related to codex project eular

---
+++ MultiplesOf3And5.py
@@ -0,0 +1,10 @@
+def mul():
+    n = int(input("how many numbers"))
+    sm = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            sm = sm + i
+    return sm
+
+
+print(mul())
