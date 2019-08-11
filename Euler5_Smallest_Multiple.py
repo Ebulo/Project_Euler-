001e0e743135f0ec5e917197f47e9fe58b2f86b8
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-08-11 23:52:12 +0530

    Euler Question 5

---
+++ Euler5_Smallest_Multiple.py
@@ -0,0 +1,10 @@
+# This one takes a lot of time but finally you will get the answer. Or I should say a LOOOOOOOOOOOOOOTTTTT..t^n of time.
+
+n = 2520        # We can increase this number to calculate it faster. 
+while(n > 0):
+    if n%1 == 0 and n%2 == 0 and n%3 == 0 and n%4 == 0 and n%5 == 0 and n%6 == 0 and n%7 == 0 and n%8 == 0 and n%9 == 0 and n%10 == 0 and n%11 == 0 and n%12 == 0 and n%13 == 0 and n%14 == 0 and n%15 == 0 and n%16 == 0 and n%17 == 0 and n%18 == 0 and n%19 == 0 and n%20 == 0:
+        print(n)
+        break
+    n+=2 
+
+# Basically we are trying to find the LCM so there should be other good ways to find it out.... I will try to find a beter way to this.
