328a833fc8efced23ff2f2451cd4d7e415ee0c4c
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-08-29 20:20:53 +0530

    Update Euler7_10001st_Prime.py

--- Euler7_10001st_Prime.py
+++ Euler7_10001st_Prime.py
@@ -1,4 +1,4 @@
-# solely taken help from youthoob, and did after understanding, I would try to develop my own way too...
+
 def nextPrime(num):
     while True:
         num+=1
@@ -14,7 +14,7 @@ def getPrime(N):
         yield num
         count += 1
 
-N = 10001                                 # This might take a lot of time but you will get the answer which is 104743
-l = [x for x in getPrime(N)]              # you can reduce the number and can check fast
+N = 10001
+l = [x for x in getPrime(N)]
 
 print(l[-1]) # answer is 104743
