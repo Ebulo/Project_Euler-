0fe74eb1e94c276cd572e3d06f1346c6c4df1570
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-09-01 00:57:06 +0530

    Update Euler5_Smallest_Multiple.py

--- Euler5_Smallest_Multiple.py
+++ Euler5_Smallest_Multiple.py
@@ -1,10 +1,76 @@
-# This one takes a lot of time but finally you will get the answer. Or I should say a LOOOOOOOOOOOOOOTTTTT..t^n of time.
+pf = []
+cf = []
+def allPrimeFactors():
+    for i in range(2, 21):
+        primeFactors(i)
+def primeFactors(n):
+    k = n
+    i = 2
+    while k > 1:
+        while (k%i == 0):
+            pf.append(i)
+            k /= i
+        i += 1
 
-n = 2520        # We can increase this number to calculate it faster. 
-while(n > 0):
-    if n%1 == 0 and n%2 == 0 and n%3 == 0 and n%4 == 0 and n%5 == 0 and n%6 == 0 and n%7 == 0 and n%8 == 0 and n%9 == 0 and n%10 == 0 and n%11 == 0 and n%12 == 0 and n%13 == 0 and n%14 == 0 and n%15 == 0 and n%16 == 0 and n%17 == 0 and n%18 == 0 and n%19 == 0 and n%20 == 0:
-        print(n)
-        break
-    n+=2 
+allPrimeFactors()
+print(pf)
 
-# Basically we are trying to find the LCM so there should be other good ways to find it out.... I will try to find a beter way to this.
+count2 = 0
+count3 = 0
+count5 = 0
+count7 = 0
+count11 = 0
+count13 = 0
+count17 = 0
+count19 = 0
+
+for x in pf:
+    if x == 2:
+        count2 += 1
+
+for x in pf:
+    if x == 3:
+        count3 += 1
+
+for x in pf:
+    if x == 5:
+        count5 += 1
+
+for x in pf:
+    if x == 7:
+        count7 += 1
+
+for x in pf:
+    if x == 11:
+        count11 += 1
+
+for x in pf:
+    if x == 13:
+        count13 += 1
+
+for x in pf:
+    if x == 17:
+        count17 += 1
+
+for x in pf:
+    if x == 19:
+        count19 += 1
+
+
+count2 //= 4
+cf.append(2**count2)
+count3 //= 4
+cf.append(3**count3)
+count5 //= 4
+cf.append(5**count5)
+count7 //= 2
+cf.append(7**count7)
+cf.append(11**count11)
+cf.append(13**count13)
+cf.append(17**count17)
+cf.append(19**count19)
+
+ans = 1
+for i in cf:
+    ans *= i
+print("The Smallest multiple is-", ans)
