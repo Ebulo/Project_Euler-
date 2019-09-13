38fa99e069a1c10ef752818a2038792cf75da770
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-09-13 09:07:28 +0530

    Update Euler6.py

--- Euler6.py
+++ Euler6.py
@@ -1,8 +1,5 @@
-square = 0
-sumSquare = 0
-for i in range(101):
-    square += i**2
-    sumSquare += i
-
-print("Square's_Sum is ", square, "\n", "Sum's_Ssquare is", sumSquare**2)
-print("And Sum's_Square - Square's_Sum is", abs(sumSquare**2 - square))
+n = 100
+sum = (n*(n+1))/2
+sum *= sum
+square = (n*(n+1)*(2*n+1))/6
+print("THE DIFFERRNCE IS - ", abs(square-sum))
