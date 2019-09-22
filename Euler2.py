5f82ae9ecde58f297290c4fc992cff1c945bba93
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-09-22 12:06:14 +0530

    Updt

--- Euler2.py
+++ Euler2.py
@@ -1,24 +1,16 @@
-
 n = 4000000
-series = [0, 1]
 
 def fib(n):
     a = 0
     b = 1
-    
+    sm = 0
     for i in range(2, n):
         c = a + b
         a = b
         b = c
         if c >= n:
             break
-        series.append(c)
-fib(n)
-
-print(series)
-sm = 0
-for i in series:
-    if i % 2 == 0:
-        sm += i
-print(sm)
-print("Total number of terms = ", len(series))
+        if c % 2 == 0:
+            sm += c
+    return sm
+print(fib(n))
