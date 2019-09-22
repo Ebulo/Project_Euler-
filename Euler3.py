8d9d6480168c2569a6833eacee86c875181fec5d
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-09-22 12:48:50 +0530

    Update Euler3.py

--- Euler3.py
+++ Euler3.py
@@ -1,11 +1,12 @@
-x = []
+x = 0
 n = 600851475143
 i = 2
+
 while n > 1:
     while(n % i == 0):
-        x.append(i)
+        x = i
         n /= i
+        
     i += 1
 
-print(x)
-print("The greater Prime Factor is", x[-1], " And the smallest being", x[0])
+print("The greatest prime number is ","\"", x,"\"", " \n Happy Coding")
