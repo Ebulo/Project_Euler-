c768bf976d46ea5678a857398636ad229d41de1e
Bishant nayak <48652521+Ebulo@users.noreply.github.com>
2019-09-22 12:56:02 +0530

    Update Euler4.py

--- Euler4.py
+++ Euler4.py
@@ -1,12 +1,12 @@
-x = []
-y = []
+x = 0
+y = 0
 for i in range(100, 1000):
     for j in range(100, 1000):
         a = i*j
         b = str(a)
         if b == b[::-1]:
             z = str(i) + '*' + str(j)
-            x.append(b)
-            y.append(z)
+            x = b
+            y = z
 
-print("The greatest number is ", x[-1], " Which is ", y[-1])
+print("The greatest number is ", x, " Which is ", y)
